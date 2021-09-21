1c1996c2865a29929b739211566128dc34da7bde
Kristian Jones <me@kristianjones.dev>
2021-09-21 20:46:21 +0000

    chore(repo): Begin work on Authentik Terraform IaC

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    #
+    # Hashicorp Consul
+    #
+    # Docs: https://registry.terraform.io/providers/hashicorp/consul/latest/docs
+    #
+    consul = {
+      source = "hashicorp/consul"
+      version = "2.13.0"
+    }
+  }
+}
