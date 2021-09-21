c930245219a526600cdee43fb7c5c6dca8081085
Kristian Jones <me@kristianjones.dev>
2021-09-21 20:59:38 +0000

    fix: TMP

--- Vault/outputs.tf
+++ Vault/outputs.tf
@@ -2,7 +2,7 @@ output "Authentik" {
   value = {
     URL = "https://auth.kristianjones.dev:8443"
 
-    Token = data.vault_generic_secret.Authentik.data["TOKEN"]
+    Token = data.vault_generic_secret.Authentik.data["PASSWORD"]
   }
 }
 
