8cf6597e0cdf072b5a14eb9c5755ef47395055ba
Kristian Jones <me@kristianjones.dev>
2021-09-22 05:20:49 +0000

    fix: IDK

--- Apps/Template/main.tf
+++ Apps/Template/main.tf
@@ -19,40 +19,40 @@ terraform {
 }
 
 
-data "authentik_flow" "default-authorization-flow" {
-  slug = "default-provider-authorization-implicit-consent"
-}
+# data "authentik_flow" "default-authorization-flow" {
+#   slug = "default-provider-authorization-implicit-consent"
+# }
 
-resource "authentik_policy_expression" "policy" {
-  name       = "example"
-  expression = "return True"
-}
+# resource "authentik_policy_expression" "policy" {
+#   name       = "example"
+#   expression = "return True"
+# }
 
-resource "authentik_policy_binding" "app-access" {
-  target = authentik_application.Application.uuid
-  policy = authentik_policy_expression.policy.id
-  order  = 0
-}
+# resource "authentik_policy_binding" "app-access" {
+#   target = authentik_application.Application.uuid
+#   policy = authentik_policy_expression.policy.id
+#   order  = 0
+# }
 
-resource "authentik_application" "Application" {
-  name = var.AppName
-  slug = "${var.AppName}-auth"
+# resource "authentik_application" "Application" {
+#   name = var.AppName
+#   slug = "${var.AppName}-auth"
 
-  protocol_provider = authentik_provider_oauth2.OID.id
-}
+#   protocol_provider = authentik_provider_oauth2.OID.id
+# }
 
-resource "random_uuid" "ClientID" {
-}
+# resource "random_uuid" "ClientID" {
+# }
 
-resource "random_password" "ClientSecret" {
-  length           = 16
-  special          = true
-  override_special = "_%@"
-}
+# resource "random_password" "ClientSecret" {
+#   length           = 16
+#   special          = true
+#   override_special = "_%@"
+# }
 
-resource "authentik_provider_oauth2" "OID" {
-  name               = var.AppName
-  client_id          = random_uuid.ClientID.result
-  client_secret      = random_password.ClientSecret.result
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-}
+# resource "authentik_provider_oauth2" "OID" {
+#   name               = var.AppName
+#   client_id          = random_uuid.ClientID.result
+#   client_secret      = random_password.ClientSecret.result
+#   authorization_flow = data.authentik_flow.default-authorization-flow.id
+# }
